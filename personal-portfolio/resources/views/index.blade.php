54acfbdd80e3b81bb3970390edd4775f247cd486
Emanuel Kywal <Kywalfilho@outlook.com>
2022-01-30 00:44:19 -0300

    Criação da estrutura html-css base

---
+++ personal-portfolio/resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+    <head>
+        <title>Portfólio</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1"> 
+        <link rel="stylesheet" type="text/css"  href="css\index_style.css">
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="area_login" id="area_externa">
+            </div>
+        </div>
+    </body>
+</html>
